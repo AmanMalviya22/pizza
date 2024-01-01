3f498f23b80c501697ebe4f1cd68fb4f9b5bb6d9
Aman Malviya <amanaman26802@gmail.com>
2024-01-01 22:05:44 +0530

    create cart page,login , sign up page

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const express=require('express')
+const express = require("express");
 
-const app=express()
-const ejs=require('ejs')
-const path=require('path')
-const expressLayouts=require('express-ejs-layouts')
-const PORT=process.env.PORT||3000
-app.get('/', (req,res)=>{
-    res.render('home')
-})
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const PORT = process.env.PORT || 3000;
 
 //assets
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 //set template engine
-app.use(expressLayouts)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
+app.use(expressLayouts);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-app.listen(PORT,(req,res)=>{
-    console.log(`server is listening on port ${PORT}`)
+//routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("./cart", (req, res) => {
+  res.render("customer/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render('./auth/register.ejs');
+});
+app.get('/cart', (req, res) => {
+    res.render('customer/cart')
 })
 
+app.listen(PORT, (req, res) => {
+  console.log(`server is listening on port ${PORT}`);
+});
