8acfc975f4f387a86080dfc21f96a50032e1fb94
Aman Malviya <amanaman26802@gmail.com>
2024-01-05 13:16:32 +0530

    added reponsiveness in FOODIES

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -37,6 +37,7 @@ export function initAdmin(socket) {
                 </td>
                 <td class="border px-4 py-2">${ order.customerId.name }</td>
                 <td class="border px-4 py-2">${ order.address }</td>
+                <td class="border px-4 py-2">${ order.phone }</td>
                 <td class="border px-4 py-2">
                     <div class="inline-block relative w-64">
                         <form action="/admin/order/status" method="POST">
